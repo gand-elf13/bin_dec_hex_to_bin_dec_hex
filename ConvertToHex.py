241e66fca80bc271f3e6ae0d410add8e81978b9c
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-09-26 21:09:02 +0200

    ConvertToHex.py
    First version of the convert_to_hex.py.
    It needs the convert_bin_to_dec() function though

---
+++ ConvertToHex.py
@@ -0,0 +1,42 @@
+def convert_remainder (remainder):
+    higher_coefs = {
+        10 : "a",
+        11 : "b",
+        12 : "c",
+        13 : "d",
+        14 : "e",
+        15 : "f",
+    }
+    if remainder < 10:
+        return str (remainder)
+    else:
+        return higher_coefs [remainder]
+
+
+def convert_dec_to_hex (n):
+    converted_n = ""
+    while True:
+        result, remainder = divmod (n, 16)
+        converted_n += convert_remainder (remainder)
+        if result == 0:
+            break
+        n = result
+    return converted_n [::-1]
+
+
+def convert_bin_to_hex (b):
+    n = int (b, 2) # WE WILL USE MAXIME'S FUNCTIONS WHEN AVAILABLE
+    return convert_dec_to_hex (n)
+
+
+# MAIN FUNCTION
+# THIS SHOULD BE THE ONLY FUNCTION THAT'S EXECUTED FROM THIS FILE!!!
+def convert_to_hex (n, base):
+    if base == "dec":
+        return convert_dec_to_hex (n)
+    elif base == "bin":
+        return convert_bin_to_hex (n)
+    elif base == "hex":
+        return n
+    else:
+        return f"ERROR : {base} IS NOT A SUPPORTED BASE YET. IT FUNCTIONS ONLY FOR bin/dec/hex BASES ONLY"
