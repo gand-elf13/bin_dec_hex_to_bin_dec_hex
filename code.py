cc22060bcb935a7e7203c675ab75f42b2282fbf3
Red-Industrie <146063004+Red-Industrie@users.noreply.github.com>
2023-09-27 10:59:39 +0200

    choisir fonction pour convertir en dec
    programme pour choisir quelle fonction choisir pour convertir en décimale.
    appel aux fonctions :
    - convert_hex_to_dec
    - convert_bin_to_dec
    - is_bin
    - is_hex

---
+++ code.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+def choose_how_convert_into_dec:
+    if n == is_hex() :
+        print(convert_hex_to_dec(n))
+    elif n == is_bin() :
+        print(convert_bin_to_dec(n))
+    else:
+        print(str(n) + " est déjà sous la forme décimale")
+    return n
