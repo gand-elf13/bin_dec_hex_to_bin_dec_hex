9864044b36aa1e1f2f3db145d7a7f2a2a689209c
gand-elf13 <duclosma@icloud.com>
2023-09-27 11:05:38 +0200

    fixed a few things

--- code_1.py
+++ code_1.py
@@ -1,9 +1,9 @@
 # -*- coding: cp1252 -*-
-def choose_how_convert_into_dec:
+def choose_how_convert_into_dec():
     if n == is_hex() :
         print(convert_hex_to_dec(n))
     elif n == is_bin() :
         print(convert_bin_to_dec(n))
     else:
-        print(str(n) + " est déjà sous la forme décimale")
+        print(str(n) + " est deja sous la forme decimale")
     return n
