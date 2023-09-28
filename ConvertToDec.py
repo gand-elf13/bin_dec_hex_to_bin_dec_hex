a85d2310fdf1e109a06cc8547567dc47c0d84394
gand-elf13 <duclosma@icloud.com>
2023-09-28 11:44:16 +0200

    added converion from binary to integer

--- ConvertToDec.py
+++ ConvertToDec.py
@@ -18,3 +18,18 @@ def convert_hex_to_int(n):
         converted_n =converted_n + (int(convert_remainder(i))*(16**power))
         power += 1
     return converted_n
+
+
+def convert_bin_to_int(n):
+    
+    converted_n = 0
+    power = 0
+
+    for i in reversed(n):
+        
+        converted_n =converted_n + (int(i)*(2**power))
+        power += 1
+    return converted_n
+
+
+print(convert_bin_to_int("10010011"))
