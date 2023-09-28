72cf09bc3db830293d8384bf59edf8c2f3099dc2
gand-elf13 <duclosma@icloud.com>
2023-09-28 11:35:31 +0200

    fixed hex to decimal conversion

--- ConvertToDec.py
+++ ConvertToDec.py
@@ -10,12 +10,13 @@ def convert_remainder (remainder):
 
 def convert_hex_to_int(n):
     
-    converted_n = ""
+    converted_n = 0
+    power = 0
 
-    for i in n:
+    for i in reversed(n):
         
-        converted_n += str(convert_remainder (i))
-
+        converted_n =converted_n + (int(convert_remainder(i))*(16**power))
+        power += 1
     return converted_n
 
-print(convert_hex_to_int("1438aef4"))
+print(convert_hex_to_int("1e"))
