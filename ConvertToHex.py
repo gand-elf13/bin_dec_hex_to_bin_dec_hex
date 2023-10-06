5e7608bc06c24abb05c34fe4e877274bd1d27dbf
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-10-06 11:29:38 +0200

    Update ConvertToHex.py
    The program is now able to convert negative bin or dec integers to hex!
    (We should start working on it for all the other modules)

--- ConvertToHex.py
+++ ConvertToHex.py
@@ -1,5 +1,14 @@
 from ConvertToDec import convert_to_dec
 
+
+'''
+reverse_str is a function that takes a string s,
+and returns it reversed, using a slicing method
+'''
+def reverse_str (s):
+    return s [::-1]
+
+
 '''
 convert_remainder is a function that takes the remainder (int),
 converts it to a hexadecimal notation, and returns it (string).
@@ -18,6 +27,7 @@ def convert_remainder (remainder):
     else:
         return higher_coefs [remainder]
 
+
 '''
 convert_dec_to_hex is a function that takes a string n
 (containing a number in decimal notation), that
@@ -26,13 +36,21 @@ returns it (as a string)
 '''
 def convert_dec_to_hex (n):
     converted_n = ""
+    is_negative = int (n) < 0
+    if is_negative:
+        n = n [1:]
     while True:
         result, remainder = divmod (int (n), 16)
         converted_n += convert_remainder (remainder)
         if result == 0:
             break
         n = result
-    return converted_n [::-1] # Reverses the string by slicing
+    
+    converted_n = reverse_str (converted_n)
+    if is_negative:
+        converted_n = "-" + converted_n
+    return converted_n
+
 
 '''
 convert_bin_to_hex is a function that takes a string b
@@ -60,3 +78,9 @@ def convert_to_hex (n, base):
         return str (n)
     else:
         return f"ERROR : {base} IS NOT A SUPPORTED BASE YET. IT FUNCTIONS ONLY FOR bin/dec/hex BASES ONLY"
+
+
+if __name__ == "__main__":
+    print(convert_to_hex("-1988971", "dec"))
+    print(convert_to_hex("1ae867f987d", "hex") == "1ae867f987d")
+    print(convert_to_hex("100100101", "bin") == "125")
