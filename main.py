24c3032049ed01b97625b12b473796b4b1efca56
gand-elf13 <145144448+gand-elf13@users.noreply.github.com>
2023-11-04 12:41:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
                                               )
             if saving == "y":
                 save()
+                print("saving succesfull.")
 
             print ("PROCESS IS NOW EXITING")
             break
